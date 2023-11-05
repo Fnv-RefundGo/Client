a9b6e9adebe5ba984056e938c82af0744c496db2
JuEun Kim <kje0405@naver.com>
2023-11-05 20:34:45 +0900

    message

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,35 +1,71 @@
-import React from 'react';
+import React from "react";
+import { Carousel } from "antd";
+import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
 import "../css/MainPage.css";
-import {Carousel} from 'antd';
-const contentStyle = {
-    margin: 0,
-    height : '160px',
-    color: '#000',
-    lineHeight : '160px',
-    textAlign : 'center',
-    background: '#364d79',
-};
 
 const MainPage = () => {
-    const onChange = (currentSlide) => {
-        console.log(currentSlide);
+    const settings = {
+        dots: true,
+        prevArrow: <CustomPrevArrow />,
+        nextArrow: <CustomNextArrow />,
     };
+
     return (
-        <div className="wrapper">
-        <Carousel autoplay afterChange={onChange}>
-            <div>
-                <h3 style={contentStyle}>1</h3>
-            </div>
-            <div>
-                <h3 style={contentStyle}>2</h3>
-            </div>
-            <div>
-                <h3 style={contentStyle}>3</h3>
-            </div>
-        </Carousel>
+        <div className="custom-carousel-container">
+            <Carousel {...settings}>
+                <div>
+                    <br/>
+                    <img className="mainImg" src={process.env.PUBLIC_URL + `/assets/refund.svg`}/>
+                    <br/>
+                    <div className="text">
+                        <div>환급Go와 함께 잊고 있던</div>
+                        <div>환금액을 찾아보세요</div>
+                    </div>
+                    <br/>
+                </div>
+                <div>
+                    <br/>
+                    <img className="mainImg" src={process.env.PUBLIC_URL + `/assets/alarm.svg`}/>
+                    <br/>
+                    <div className="text">
+                        <div>환급신청 잊어도 걱정마세요.</div>
+                        <div>환금Go가 챙겨드려요!</div>
+                    </div>
+                    <br/>
+                </div>
+                <div>
+                    <br/>
+                    <img className="mainImg" src={process.env.PUBLIC_URL + `/assets/clock.svg`}/>
+                    <br/>
+                    <div className="text">
+                        <div>1분이면 신청완료!</div>
+                        <div>환급Go와 환급액 찾기</div>
+                        <div>시작해볼까요?</div>
+                    </div>
+                    <br/>
+                </div>
+            </Carousel>
+            <img className="nextBtn" src={process.env.PUBLIC_URL + `/assets/next button.svg`}/>
         </div>
     );
 };
+
+const CustomPrevArrow = (props) => {
+    const { className, onClick } = props;
+    return (
+        <div className={`custom-carousel-button custom-prev ${className}`} onClick={onClick}>
+            <LeftCircleOutlined />
+        </div>
+    );
+};
+
+const CustomNextArrow = (props) => {
+    const { className, onClick } = props;
+    return (
+        <div className={`custom-carousel-button custom-next ${className}`} onClick={onClick}>
+            <RightCircleOutlined />
+        </div>
+    );
+};
+
 export default MainPage;
-
-
