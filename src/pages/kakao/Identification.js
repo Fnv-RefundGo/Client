d0177d1ada9ad6cadddb53465837950a69825672
JuEun Kim <kje0405@naver.com>
2023-11-09 11:31:21 +0900

    카카오 로그인 본인인증 페이지 구현 완료

--- src/pages/kakao/Identification.js
+++ src/pages/kakao/Identification.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Button} from "antd";
+import "../../css/Identification.css";
 
 const Identification = () => {
     return(
         <div>
-            <Button>뒤로가기</Button>
-            <div>본인확인</div>
-            <div>카카오 계정으로 로그인하여</div>
-            <div>세액 공제 환급 대상을 확인하세요.</div>
+            <Button className="backBtn"> <a href="/"> > 뒤로가기 </a></Button>
+            <div className="kakao-title">본인확인</div>
+            <div className="kakao-text1">카카오 계정으로 로그인하여</div>
+            <div className="kakao-text2">세액 공제 환급 대상을 확인하세요.</div>
+            <div className="kakao-loginBtn">
+                <img className="kakao-logo" src={process.env.PUBLIC_URL + `/assets/kakao_logo.png`}/>
+                <div className="kakao-text3">카카오 계정으로 로그인</div>
+            </div>
         </div>
     );
 }
