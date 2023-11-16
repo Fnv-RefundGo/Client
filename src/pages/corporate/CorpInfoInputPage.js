19533995a3d00c3823d930de86cde5ed2dd2bb84
JuEun Kim <kje0405@naver.com>
2023-11-16 17:54:52 +0900

    법인사업자 사업장 정보 조회 및 담당자 변경 구현

--- src/pages/corporate/CorpInfoInputPage.js
+++ src/pages/corporate/CorpInfoInputPage.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import "../../css/InfoInputPage.css";
 import "../../css/CorpInfoInputPage.css";
-import {Button, Input, Popover, Steps} from "antd";
+import {Button, Input, Popover, Steps, Modal} from "antd";
 
 const customDot = (dot) => (
     <Popover>{dot}</Popover>
@@ -13,6 +13,10 @@ const CorpInfoInputPage = () => {
     const [businessNumSec, setBusinessNumSec] = useState('');
     const [businessNumTrd, setBusinessNumTrd] = useState('');
     const [errors, setErrors] = useState({});
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isManagerModalOpen, setIsManagerModalOpen] = useState(false);
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
 
     // 상호명 검증
     const validateCorpName = () => {
@@ -47,6 +51,49 @@ const CorpInfoInputPage = () => {
 
     const isFormValid = corpName.length >= 1 && /^[a-zA-Z가-힣]+$/.test(corpName)
         && /^\d{3}$/.test(businessNumFst) && /^\d{2}$/.test(businessNumSec) && /^\d{5}$/.test(businessNumTrd);
+
+    // 모달
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    // 담당자 변경 모달
+    const showManagerModal = () => {
+        setIsManagerModalOpen(true);
+    };
+    const ManagerHandleOk = () => {
+        setIsManagerModalOpen(false);
+    };
+    const ManagerHandleCancel = () => {
+        setIsManagerModalOpen(false);
+    };
+
+    // 담당자 이름, 연락처 검증
+    const validateName = () => {
+        const nameRegex = /^[a-zA-Z가-힣]+$/; // 이름에는 알파벳과 한글만 허용
+        if (!nameRegex.test(name)) {
+            setErrors((prevErrors) => ({ ...prevErrors, name: '올바른 이름을 입력바랍니다.' }));
+        } else if(name.length < 2){
+            setErrors((prevErrors) => ({ ...prevErrors, name: '2글자 이상 입력바랍니다.' }));
+        } else {
+            setErrors((prevErrors) => ({ ...prevErrors, name: '' }));
+        }
+    };
+
+    const validatePhone = () => {
+        const phoneRegex = /^\d{3}\d{4}\d{4}$/;
+        if (!phoneRegex.test(phone) || !/^010|011|016|017|018|019/.test(phone.substring(0, 3))) {
+            setErrors((prevErrors) => ({ ...prevErrors, phone: '올바른 휴대폰 번호를 입력바랍니다.' }));
+        } else {
+            setErrors((prevErrors) => ({ ...prevErrors, phone: '' }));
+        }
+    };
 
     return(
         <div>
@@ -106,9 +153,75 @@ const CorpInfoInputPage = () => {
                     />
                     {errors.num && <div className="error-message">{errors.num}</div>}
                 </div>
-                <Button className="nextButton" disabled={!isFormValid}>
+                <Button className="nextButton" disabled={!isFormValid} onClick={showModal}>
                     다음
                 </Button>
+                <Modal
+                    visible={isModalOpen}
+                    onOk={handleOk}
+                    onCancel={handleCancel}
+                    centered
+                    width="400px"
+                    style={{textAlign:'center',} }
+                    footer={[
+                        <div style={{display:'flex'}}>
+                        <Button style={{ margin: '0 auto', backgroundColor: '#DCDCDC', width: '150px' }} key="ok" onClick={handleOk}>
+                            닫기
+                        </Button>
+                        <Button style={{ margin: '0 auto', backgroundColor: '#4A5BF6', color: 'white', width: '150px',}} onClick={showManagerModal}>
+                            담당자 변경
+                        </Button>
+                        </div>
+                    ]}
+                >
+                    <h3>이미 환급 조회 신청을 한 사업장입니다.</h3>
+                </Modal>
+
+                <Modal
+                    title="담당자 변경하기"
+                    visible={isManagerModalOpen}
+                    onOk={ManagerHandleOk}
+                    onCancel={ManagerHandleCancel}
+                    centered
+                    width="400px"
+                    footer={[
+                        <div style={{display:'flex'}}>
+                            <Button style={{ margin: '0 auto', backgroundColor: '#DCDCDC', width: '150px', }} key="ok" onClick={ManagerHandleOk}>
+                                닫기
+                            </Button>
+                            <Button style={{ margin: '0 auto', backgroundColor: '#4A5BF6', color: 'white', width: '150px',}}>
+                                변경하기
+                            </Button>
+                        </div>
+                    ]}
+                >
+                    <div className={`hometax-name ${errors.name ? 'error' : ''}`}>
+                        이름
+                        <Input
+                            className={errors.name ? 'error-border' : ''}
+                            placeholder="홍길동"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            onBlur={() => {
+                                validateName();
+                            }}
+                        />
+                        {errors.name && <div className="error-message">{errors.name}</div>}
+                    </div>
+                    <div className={`hometax-phone ${errors.phone ? 'error' : ''}`}>
+                        휴대폰번호<br />
+                        <Input
+                            className={errors.phone ? 'error-border' : ''}
+                            placeholder="010 1234 5678"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            onBlur={() => {
+                                validatePhone();
+                            }}
+                        />
+                        {errors.phone && <div className="error-message">{errors.phone}</div>}
+                    </div>
+                </Modal>
             </div>
         </div>
     );
