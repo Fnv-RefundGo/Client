e4d8be7aa2d9075a8e7315ece0b80bee6fe72326
JuEun Kim <kje0405@naver.com>
2023-11-21 17:47:12 +0900

    개인사업자 홈택스 카카오 간편인증 연동 시도

--- src/pages/individual/hometax/HometaxIndiCertificationPage.js
+++ src/pages/individual/hometax/HometaxIndiCertificationPage.js
@@ -1,29 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import '../../../css/InfoInputPage.css';
-import '../../../css/CertificationPage.css';
-import {Button, Modal, Popover, Steps} from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Button, Modal, Popover, Steps } from 'antd';
+import axios from 'axios';
 
 const customDot = (dot) => (
     <Popover>{dot}</Popover>
 );
 
 const HometaxIndiCertificationPage = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    // 카카오 메시지 전송 함수
+    const handleKakaoMessage = async () => {
+        try {
+            const response = await axios.get("/sendKakaoMessage");
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error sending Kakao message:', error);
+        }
+    };
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
     const [minutes, setMinutes] = useState(5);
     const [seconds, setSeconds] = useState(0);
 
     const showModal = () => {
+        handleKakaoMessage(); // 모달이 열릴 때 Kakao 메시지 전송 함수 호출
         resetTimer();
-        setIsModalOpen(true);
+        setIsModalVisible(true);
     };
 
     const handleOk = () => {
-        setIsModalOpen(false);
+        setIsModalVisible(false);
     };
 
     const handleCancel = () => {
-        setIsModalOpen(false);
-        window.location.href = "/comwel-individual-infoInput";
+        setIsModalVisible(false);
     };
 
     const resetTimer = () => {
@@ -31,6 +41,7 @@ const HometaxIndiCertificationPage = () => {
         setSeconds(0);
     };
 
+    // 타이머 로직
     useEffect(() => {
         const countdown = setInterval(() => {
             if (parseInt(seconds) > 0) {
@@ -58,81 +69,85 @@ const HometaxIndiCertificationPage = () => {
                     items={[{title: '',}, {title: '',}, {title: '',}, {title: '',},]}
                 />
             </div>
-        <div className="wrapper">
-            <a href="/hometax-individual-infoInput">
-                <img className="backB" src={process.env.PUBLIC_URL + `/assets/back_button.png`} />
-            </a>
-            <br />
-            <br />
-            <br />
-            <div className="hometax-text1">
-                <span className="kakao-cert">카카오 간편인증</span>으로
+            <div className="wrapper">
+                <a href="/hometax-individual-infoInput">
+                    <img className="backB" src={process.env.PUBLIC_URL + `/assets/back_button.png`} />
+                </a>
                 <br />
-                빠르게 환급액을 확인해드릴게요.
-            </div>
-            <div className="hometax-text2">
-                세금 환급액 조회를 위해 카카오 간편인증이 필요합니다.
-            </div>
-            <img
-                className="hometax-cert"
-                src={process.env.PUBLIC_URL + `/assets/hometax_cert.png`}
-            />
-            <Button className="certButton" onClick={showModal}>
-                홈택스 인증요청
-            </Button>
-            <Modal
-                width="400px"
-                visible={isModalOpen}
-                onOk={handleOk}
-                onCancel={handleCancel}
-                centered
-                footer={[
-                    <Button
-                        className={`certification-button ${
-                            minutes !== 0 || seconds !== 0 ? 'disabled' : ''
-                        }`}
-                        style={{
-                            margin: '0 auto',
-                            display: 'block',
-                            backgroundColor: '#4A5BF6',
-                            color: 'white',
-                            width: '350px',
-                            height: '50px',
-                        }}
-                        disabled={minutes !== 0 || seconds !== 0}
-                        key="ok"
-                        onClick={handleOk}
-                    >
-                        재인증 요청
-                    </Button>,<br/>
-                ]}
-            >
-                <div className="kakao-cert-text">
-                    <h4>카카오톡으로 인증 요청 메세지를 보냈습니다.<br/>
-                        인증시간 만료 시 재인증 요청 버튼을 눌러 재시도 바랍니다.
-                    </h4>
+                <br />
+                <br />
+                <div className="hometax-text1">
+                    <span className="kakao-cert">카카오 간편인증</span>으로
+                    <br />
+                    빠르게 환급액을 확인해드릴게요.
                 </div>
-                <div className="kakao-cert-timer">
-                    {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-                </div><br/>
-                <div className="modal-contents">
-                    <img className="cert-img"src={process.env.PUBLIC_URL + `/assets/certMsg.svg`}/>
-                    <div className="cert-msg">
-                        <b>STEP 1.</b><br/>
-                        카카오톡에서 인증요청 메세지 확인
+                <div className="hometax-text2">
+                    세금 환급액 조회를 위해 카카오 간편인증이 필요합니다.
+                </div>
+                <img
+                    className="hometax-cert"
+                    src={process.env.PUBLIC_URL + `/assets/hometax_cert.png`}
+                />
+                <Button className="certButton" onClick={showModal}>
+                    홈택스 인증요청
+                </Button>
+                <Modal
+                    width="400px"
+                    visible={isModalVisible}
+                    onOk={handleOk}
+                    onCancel={handleCancel}
+                    centered
+                    footer={[
+                        <Button
+                            className={`certification-button ${
+                                minutes !== 0 || seconds !== 0 ? 'disabled' : ''
+                            }`}
+                            style={{
+                                margin: '0 auto',
+                                display: 'block',
+                                backgroundColor: '#4A5BF6',
+                                color: 'white',
+                                width: '350px',
+                                height: '50px',
+                            }}
+                            disabled={minutes !== 0 || seconds !== 0}
+                            key="ok"
+                            onClick={handleOk}
+                        >
+                            재인증 요청
+                        </Button>,
+                    ]}
+                >
+                    <div className="kakao-cert-text">
+                        <h4>카카오톡으로 인증 요청 메세지를 보냈습니다.<br/>
+                            인증시간 만료 시 재인증 요청 버튼을 눌러 재시도 바랍니다.
+                        </h4>
                     </div>
-                </div><br/>
-                <div className="next-icon"> v </div>
-                <div className="modal-contents">
-                    <img className="cert-img" src={process.env.PUBLIC_URL + `/assets/lock.svg`}/>
-                    <div className="cert-msg">
-                        <b>STEP 2.</b><br/>
-                        카카오톡에서 간편인증 진행하기
+                    <div className="kakao-cert-timer">
+                        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
                     </div>
-                </div><br/><br/>
-            </Modal>
-        </div>
+                    <br />
+                    <div className="modal-contents">
+                        <img className="cert-img"src={process.env.PUBLIC_URL + `/assets/certMsg.svg`}/>
+                        <div className="cert-msg">
+                            <b>STEP 1.</b><br/>
+                            카카오톡에서 인증요청 메세지 확인
+                        </div>
+                    </div>
+                    <br />
+                    <div className="next-icon"> v </div>
+                    <div className="modal-contents">
+                        <img className="cert-img" src={process.env.PUBLIC_URL + `/assets/lock.svg`}/>
+                        <div className="cert-msg">
+                            <b>STEP 2.</b><br/>
+                            카카오톡에서 간편인증 진행하기
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                </Modal>
             </div>
+        </div>
     );
 };
 
